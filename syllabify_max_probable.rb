9c3e425cd393ddd55217737d437a8dc610dc9635
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-07 10:12:47 +0530

    add method to find all possible syllabifications from a word

--- syllabify_max_probable.rb
+++ syllabify_max_probable.rb
@@ -41,3 +41,93 @@ def hyphenate(word)
 end
 
 
+
+def possible_syllabifications(word)
+
+	#######
+	hyphenated_word = hyphenate(word)
+	hyphenated_word.split("-").select{|split| not split[0].match(/[aeiou]/).to_a.empty?}
+	clusters = hyphenated_word.split("-")
+	vowel_clusters = hyphenated_word.split("-").select{|split| not split[0].match(/[aeiou]/).to_a.empty?}
+
+	vowel_clusters_starting_indices = []
+	traversed = 0
+	clusters.each_with_index do |cluster , i|
+		if not cluster.match(/[aeiou]/).to_a.empty?
+			vowel_clusters_starting_indices << traversed
+		end
+		traversed += cluster.size
+	end
+
+	first_vowel_index = vowel_clusters_starting_indices.first
+	last_vowel_index = vowel_clusters_starting_indices.last
+
+	if vowel_clusters_starting_indices.size <= 1
+		hyphenated_word = word
+		$pre = ""
+		$post = ""
+	else
+		$pre = word.slice(0 , first_vowel_index + 1)
+		$post = word.slice( last_vowel_index , word.size - last_vowel_index)
+		mid = (word.slice(first_vowel_index + 1 , last_vowel_index - first_vowel_index - 1))
+		hyphenated_word = word
+		mid = hyphenate(mid)
+		hyphenated_word = [$pre , mid , $post ].join("-")
+	end
+	return [hyphenated_word] if hyphenated_word.split("-").size == 1
+	#######
+
+	sum = 0
+	cumulative_sizes = hyphenated_word.split("-").map{|x| x.size }.map{|size| sum = sum + size  }
+	cumulative_sizes.pop
+
+	confusing_parts = []
+	while cumulative_sizes.size >= 2 do
+		confusing_parts << cumulative_sizes.take(2)
+		cumulative_sizes.shift
+	end
+
+	confusing_parts.each{|confusing_part| confusing_part[0] += -1}
+	confusing_parts = confusing_parts.map{|confusing_part| ((confusing_part.first)..(confusing_part.last)).to_a.combination(2).to_a.select{|confusing_part| confusing_part.first == confusing_part.last - 1} }
+	confusing_consonants = confusing_parts.select{|confusing_part| is_consonant(word[confusing_part[1][0]])}
+	confusing_vowels = confusing_parts.select{|confusing_part| is_vowel(word[confusing_part[1][0]])}
+
+	# ap confusing_consonants
+
+	if confusing_consonants.size >= 2
+		# ap confusing_consonants.inspect
+		possible_split_combinations = array_permutation(confusing_consonants).map{|x| x.flatten}.map{|x| x.each_slice(2).to_a }
+		# ap possible_split_combinations#.inspect
+		original_word = word
+		possible_syllabifications = []
+		# ap possible_split_combinations
+		for split_combination in possible_split_combinations
+			word = original_word.dup
+			added = 0
+			split_combination.each do |split|
+				word = word.dup
+				word.insert(split.last + added , "-")
+				added += 1
+			end
+			possible_syllabifications << word
+		end
+		return possible_syllabifications
+	elsif confusing_consonants.size == 1
+		possible_splits = confusing_consonants[0]
+		original_word = word.dup
+		possible_syllabifications = []
+		for split in possible_splits
+			word = original_word.dup
+			word.insert(split.last , "-")
+			possible_syllabifications << word
+		end		
+		return possible_syllabifications
+	else
+		return [word]
+	end
+
+end
+
+
+
+
