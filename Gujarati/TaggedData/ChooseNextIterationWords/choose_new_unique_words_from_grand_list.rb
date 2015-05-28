96eef81e26f0e80539c61701168c00d0876a38c7
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-28 10:32:19 +0530

    Add script and csvs for choosing new and unique words from a grand word list on every iteration

---
+++ Gujarati/TaggedData/ChooseNextIterationWords/choose_new_unique_words_from_grand_list.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+require 'bundler/setup'
+Bundler.require
+
+## Using Version 1 of data for for getting new words
+
+directory = __dir__
+
+all_words = CSV.read( File.join( __dir__ , "grand_word_list.csv"  ) ).flatten(1).uniq
+
+done_words_hash = {}
+word_syllabifications = CSV.read( File.join( Bundler.root , "TaggedData" , "version-1" , "tagged_data.csv" ) ).each{|x| done_words_hash[x.first.strip] = true }
+
+new_words = all_words.select{|word| not done_words_hash[word.strip] }
+
+chosen_words = new_words.uniq.shuffle.sample( 14000 )
+
+CSV.open(   File.join( __dir__ , "to_be_suggested_data.csv" )   , 'w') do |csv_object|
+  chosen_words.map{|x| [x]}.each { |row_array| csv_object << row_array }
+end
