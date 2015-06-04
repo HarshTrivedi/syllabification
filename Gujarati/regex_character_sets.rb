e68cf9586ab8cb49372291c399d3e5dab85d910c
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-06-04 21:30:39 +0530

    update character_details and regex sets for gujarati

--- Gujarati/regex_character_sets.rb
+++ Gujarati/regex_character_sets.rb
@@ -2,13 +2,16 @@
 # (For easy searching regex searching in Gujarati)
 
 # 1. Matras
-$guj_matra_regex = Regexp.new(/[ાિીુૂૅૅૈોૉૌં્]/)
+$guj_matra_regex = Regexp.new(/[ાિીુૂૅેૈોૉૌૃૄ્ઁંઃ઼ઽ]/)
 
 # 2. Vowel
 $guj_vowel_regex = Regexp.new(/[અઆઇઈઉઊએઍઐઓઑઔ]/)
 
 # 3. Consonants
-$guj_consonant_regex = Regexp.new(/[કખગઘઙચછજઝઞટઠડઢણતથદધનપફબમયરલવશષસહળ]/)
+$guj_consonant_regex = Regexp.new(/[કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ]/)
 
 # 4. All Gujarati Characters
-$guj_all_regex = Regexp.new(/[કખગઘઙચછજઝઞટઠડઢણતથદધનપફબમયરલવશષસહળઅઆઇઈઉઊએઍઐઓઑઔાિીુૂૅૅૈોૉૌં્]/)
+$guj_all_regex = Regexp.new(/[કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળઅઆઇઈઉઊએઍઐઓઑઔાિીુૂૅેૈોૉૌૃૄ્ઁંઃ઼ઽ]/)
+
+# 5. Non Gujaratic alphabets (gujarati numbers excluded)
+$non_guj_regex = Regexp.new(/[^કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળઅઆઇઈઉઊએઍઐઓઑઔાિીુૂૅેૈોૉૌૃૄ્ઁંઃ઼ઽ]/)
